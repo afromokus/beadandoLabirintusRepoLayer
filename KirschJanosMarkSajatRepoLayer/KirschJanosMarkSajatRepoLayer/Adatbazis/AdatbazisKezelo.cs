7edd20a8ec1c3941784063c2df7593683a458530
unknown <kirschjanosmark@gmail.com>
2019-02-05 18:35:58 +0100

    csatlakozás megkezdése, sikeres csatlakozás

--- KirschJanosMarkSajatRepoLayer/KirschJanosMarkSajatRepoLayer/Adatbazis/AdatbazisKezelo.cs
+++ KirschJanosMarkSajatRepoLayer/KirschJanosMarkSajatRepoLayer/Adatbazis/AdatbazisKezelo.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace KirschJanosMarkSajatRepoLayer.Adatbazis
 {
     class AdatbazisKezelo
     {
+        MySqlConnection csatlakozas;
+        MySqlCommand parancs;
+
+
+        public AdatbazisKezelo()
+        {
+            csatlakozas = new MySqlConnection();
+            parancs = new MySqlCommand("");
+
+            csatlakozas.ConnectionString = "SERVER = 127.0.0.1; DATABASE = lab3d; Uid = root; pwd = ; port = 3306";
+            csatlakozas.Open();
+            csatlakozas.Close();
+
+        }
     }
 }
--- KirschJanosMarkSajatRepoLayer/KirschJanosMarkSajatRepoLayer/Form1.cs
+++ KirschJanosMarkSajatRepoLayer/KirschJanosMarkSajatRepoLayer/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KirschJanosMarkSajatRepoLayer.Adatbazis;
 
 namespace KirschJanosMarkSajatRepoLayer
 {
@@ -15,6 +16,11 @@ namespace KirschJanosMarkSajatRepoLayer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AdatbazisKezelo ak = new AdatbazisKezelo();
         }
     }
 }
